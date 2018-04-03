92b6ad251a24d890df156e6dd571b1155cab92d9
Ian Otieno <ian.otieno@andela.com>
2018-04-03 18:51:15 +0300

    bring repo up to speed with local

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import { Card, CardHeader, CardTitle, CardText, CardContent } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 
 
 import * as issueTypes from './constants/actionTypes';
@@ -14,19 +19,56 @@ class App extends Component {
     fetchIssues();
   }
 
-  render() {
+  handleSubmitIssue() {
+    // code here to call action used to creat a new issue
+  }
+
+  renderIssueCards() {
     const { allIssues } = this.props;
-    console.log('all issues here', this.props);
+
+    const cardedIssue = allIssues.map(issue => {
+      return (
+        <div>
+          <Card
+            style={{ width: '40%' }}
+          >
+            <CardHeader
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+            <CardTitle subtitle={issue.get('assigned')} />
+            <CardText expandable={true}>
+              {issue.get('description')}
+            </CardText>
+          </Card>
+        </div>
+      );
+    });
+
+    return cardedIssue;
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Issue Tracker</h1>
+          </header>
+          <div>
+            <TextField
+              hintText="Type description here"
+            />
+            <TextField
+              hintText="Set the assigned person here"
+            />
+          </div>
+          <div>
+            {this.renderIssueCards()}
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
